e2067b40ec4b4242dc3d51edf48c1b78569e60da
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-03 17:26:58 -0700

    create BooleanLogic program for drill

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //Console.WriteLine("Your favorite number plus 5 is: " + (favNum + 5));
+            //Console.Read();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char randomLetter = 'X';
+            char randUni = '\u0021';
+            decimal balance = 500.50m;
+            double heightInCent = 211.3020310;
+            float secondsLeft = 2.62f;
+            short tempOnMars = -341;
+            string myName = "Austin";
+
+            int currentAge = 23;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = isRaining.ToString();
+
+        }
+    }
+}
