ce6d70a0a1d3b8be8fded2e20e9b6aa4616c5bb3
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-03 23:33:24 -0700

    create branching exersice project

---
+++ BranchingExercise/BranchingExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BranchingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight >= 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width: ");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height: ");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length: ");
+                int length = Convert.ToInt32(Console.ReadLine());
+                int sum = width + length + height;
+                if (sum > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                    Console.Read();
+                }
+                else
+                {
+                    decimal total = (sum * weight) / 100.00m + 0.00m;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + total+"\nThank You!");
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
