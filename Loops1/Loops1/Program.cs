bb82152a06d815ba4893028e74d3924b8d2c7dc6
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-06 18:46:46 -0700

    made file for the first loops drill

---
+++ Loops1/Loops1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Loops1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this counts by 1 from 0 all the numbers less than 10
+            int z = 0;
+            while (z < 10)
+            {
+                Console.WriteLine(z);
+                z++;
+            }
+            //this counts by 1 all the numbers less than 10, since x already is 10 without the 'do' this wouldn't print anything
+            int x = 10;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            while (x < 10);
+            Console.Read();
+        }
+    }
+}
