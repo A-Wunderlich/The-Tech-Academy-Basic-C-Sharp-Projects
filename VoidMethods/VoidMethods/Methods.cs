58685d49246f2db1bbee1d0b401bc136caecf15d
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-10 21:32:23 -0700

    created a project for demoing calling methods of classes

---
+++ VoidMethods/VoidMethods/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoidMethods
+{
+    public class Methods
+    {
+        
+        
+
+        public void method(int multi, int display)
+        {
+            multi = multi * 5;
+            Console.WriteLine(display);
+        }
+    }
+}
---
+++ VoidMethods/VoidMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VoidMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods m = new Methods();
+            m.method(multi: 10, display: 25);
+        }
+    }
+}
