6a13e43f2cf72dbc0443596839528d131d4d902f
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-10 21:51:59 -0700

    create a project to demo overloads of methods

---
+++ Overloads/Overloads/Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Overloads
+{
+    public class Methods
+    {
+        public int timesX(int num)
+        {
+            return num * 5;
+        }
+        public decimal timesX(decimal num)
+        {
+            return num * 3;
+        }
+
+        public int timesX(string num)
+        {
+            int numInt = Convert.ToInt32(num);
+            return numInt * 13;
+        }
+
+    }
+}
---
+++ Overloads/Overloads/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Overloads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods multi = new Methods();
+            int solutionOne = multi.timesX(12);
+            Console.WriteLine(solutionOne);
+            decimal solutionTwo = multi.timesX(5.5m);
+            Console.WriteLine(solutionTwo);
+            int solutionThree = multi.timesX("13");
+            Console.WriteLine(solutionThree);
+            
+        }
+    }
+}
