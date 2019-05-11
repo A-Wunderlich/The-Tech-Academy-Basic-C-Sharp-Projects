7fefcdf3d2e6d78cee34108b7d81fbdf847f1cfd
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-10 18:40:48 -0700

    create project for classes and methods drill

---
+++ Classes/Classes/Operation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Operation
+    {
+        
+        public static int Mult2(int num)
+        {
+            return num * 2;
+        }
+        public static int Add100(int num)
+        {
+            return num + 100;
+        }
+        public static int MultAdd(int num)
+        {
+            return (num + 100) * 2;
+        }
+
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to do the math operations on?");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int pro = Operation.Mult2(num);
+            Console.WriteLine("Multiplied by 2 the product is: " + pro);
+            int sum = Operation.Add100(num);
+            Console.WriteLine("With 100 added to it the sum is: " + sum);
+            int sumPro = Operation.MultAdd(num);
+            Console.WriteLine("Your number added to 100 THEN multiplied by 2 is: " + sumPro + "!");
+            Console.ReadLine();
+        }
+    }
+}
