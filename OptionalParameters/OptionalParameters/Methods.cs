19355e774584e2f76eaa1508419ae84505a116f5
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-13 17:52:18 -0700

    create project for optional parameters

---
+++ OptionalParameters/OptionalParameters/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionalParameters
+{
+    public class Methods
+    {
+        public int Math(int numOne, int numTwo = 2)
+        {
+            return (numOne + 12) * (numTwo + 15);
+        }
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods m = new Methods();
+            Console.WriteLine("Please enter a number to used in mathmatic operation.");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number to used in mathmatic the operation.(You do not have to enter a number here)");
+            string numTwoStr = Console.ReadLine();
+            if (numTwoStr == "")
+            {
+                int test = m.Math(numOne);
+                Console.WriteLine("The result is: {0}", test);
+            }
+            else
+            {
+                int numTwo = Convert.ToInt32(numTwoStr);
+                int test = m.Math(numOne,numTwo);
+                Console.WriteLine("The result is: {0}", test);
+            }
+        }
+
+    }
+}
