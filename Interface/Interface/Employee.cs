e61cf1d355b13746978a514fd09640e74b71b9af
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-15 07:48:08 -0700

    create project for interface demo

---
+++ Interface/Interface/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("THEY QUIT!!! Quiter...");
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            IQuittable employeeQuit = new Employee();
+            employeeQuit.Quit();
+            Console.ReadLine();
+
+
+        }
+    }
+}
