7f7febd9f513cf0eba9368082c1d5dc9b41b673a
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-14 20:08:30 -0700

    create project for method outputs

---
+++ MethodOutputs/MethodOutputs/Method.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOutputs
+{
+    public class Method
+    {
+        public void Divide(int number, out int div)
+        {
+            div = number / 2;
+        }
+        public void Addition(out int p, out int q)
+        {
+            p = 30;
+            q = 40;
+            p += p;
+            q += q;
+        }
+
+        public void Addition(out decimal p, out decimal q)
+        {
+            p = 12.25m;
+            q = 16.33m;
+            p += p;
+            q += q;
+        }
+
+    }
+
+}
---
+++ MethodOutputs/MethodOutputs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodOutputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method m = new Method();
+            Console.WriteLine("Please choose a number to be divided.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int div;
+            m.Divide(number,out div);
+            Console.WriteLine("After method call, the value is : {0}", div);
+            int a, b;
+            m.Addition(out a, out b);
+            Console.WriteLine("The addition of the value is: {0}", a);
+            Console.WriteLine("The addition of the value is: {0}", b);
+            decimal c, d;
+            m.Addition(out c, out d);
+            Console.WriteLine("The addition of the value is: {0}", c);
+            Console.WriteLine("The addition of the value is: {0}", d);
+        }
+    }
+}
