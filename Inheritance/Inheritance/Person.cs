c75df349ac26543e10364bfcca74c15c44504a09
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-14 22:59:01 -0700

    create project for inheritance

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine("Name: {0} {1}", FirstName,LastName);
+        }
+    }
+}
