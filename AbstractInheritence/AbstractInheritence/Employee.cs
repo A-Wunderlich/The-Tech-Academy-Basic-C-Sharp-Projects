0efc497d5dd8d030209714494739acb9a0d65ce6
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-15 20:59:28 -0700

    added a == overload to abstract inheritance project

--- AbstractInheritence/AbstractInheritence/Employee.cs
+++ AbstractInheritence/AbstractInheritence/Employee.cs
@@ -6,9 +6,35 @@ namespace AbstractInheritence
 {
     class Employee : Person
     {
+        public int Id { get; set; }
         public override void SayName()
         {
             base.SayName();
+        }
+
+        public static Boolean operator==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+        public static Boolean operator !=(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
--- AbstractInheritence/AbstractInheritence/Program.cs
+++ AbstractInheritence/AbstractInheritence/Program.cs
@@ -8,6 +8,10 @@ namespace AbstractInheritence
         {
             Employee employee = new Employee() {FirstName = "Sample",LastName = "Student" };
             employee.SayName();
+            Employee employee1 = new Employee() { Id = 555 };
+            Employee employee2 = new Employee() { Id = 555 };
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
 
 
         }
