09668d98541f80aa938b31971c6e4ed458902319
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-20 18:34:35 -0700

    create project for practicing creating structs

---
+++ Structs/Structs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number dec = new Number();
+            dec.Amount = 5.55m;
+            Console.WriteLine(dec.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
