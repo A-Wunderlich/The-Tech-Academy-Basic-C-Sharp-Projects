4732145c5a89a9bcdbb6a3462622d7d29366341a
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-24 23:29:28 -0700

    create project for C# features demo

---
+++ CSharpFeatures/CSharpFeatures/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFeatures
+{
+    class Employee
+    {
+        public Employee(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public Employee(string name) : this(name,400)
+        {
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CSharpFeatures/CSharpFeatures/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "George" ;
+            //name = "Georgie";
+            //this will not work because it is a constant
+
+            var caps = new StringDictionary()
+            {
+                ["Oregon"] = "Salem",
+                ["Washington"] = "Olympia",
+                ["California"] = "Sacramento"
+
+            };
+            foreach(DictionaryEntry de in caps)
+            {
+                Console.WriteLine(de.Value);
+            }
+            Employee employee = new Employee("Austin", 101);
+            Employee employee2 = new Employee("James");
+            Console.WriteLine("{0} {1}\n{2} {3}",employee.Name,employee.Id,employee2.Name,employee2.Id);
+            Console.WriteLine(name + " is a curious one");
+            Console.ReadLine();
+
+        }
+        
+    }
+}
