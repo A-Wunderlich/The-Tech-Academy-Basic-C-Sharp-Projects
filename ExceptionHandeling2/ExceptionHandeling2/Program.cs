4ca6a90a1fb12c4a6a8affb208f5fdfcd64a5b00
Austin Wunderlich <WunderlichAustin@gmail.com>
2019-05-28 21:14:40 -0700

    create project for custom exceptions and error handling

---
+++ ExceptionHandeling2/ExceptionHandeling2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandeling2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 0;
+
+            int year = DateTime.Today.Year;
+            bool validAnswer = false;
+            while (!validAnswer)
+            {
+                Console.WriteLine("How old are you?");
+                validAnswer = int.TryParse(Console.ReadLine(), out age);
+                if (!validAnswer) Console.WriteLine("Please enter whole intigers only.");
+            }
+            try
+            {
+                if (age <= 0)
+                {
+                    throw new AgeException();
+                }
+                else
+                {
+                    Console.WriteLine("The year you were born was: {0}", (year - age));
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("The age you entered is invalid and/or impossible");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An error has occured. Please contact your system admin.");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
